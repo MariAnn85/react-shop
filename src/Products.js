803884ffb402f003f3afa77f2982d64af1f61ce3
MariAnn85 <75684730+MariAnn85@users.noreply.github.com>
2023-02-21 01:28:32 +0400

    Add files via upload

---
+++ src/Products.js
@@ -0,0 +1,5 @@
+import React from "react";
+import styled from "styled-components";
+const Products = () =>  {
+  return <h1>Products</h1>
+};
---
+++ src/SingleProduct.js
@@ -0,0 +1,5 @@
+import styled from "styled-components";
+
+const SingleProduct = () =>  {
+  return <h1>SingleProduct</h1>
+};
