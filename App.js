6cd8b8a121c87f8cd2ff56a526d93f4c6964a89a
MariAnn85 <75684730+MariAnn85@users.noreply.github.com>
2023-02-20 21:42:27 +0400

    Create App.js

---
+++ App.js
@@ -0,0 +1,32 @@
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Products from "./Products";
+import Contact from "./Contact";
+import Cart from "./Cart";
+import SingleProduct from "./SingleProduct";
+import ErrorPage from "./ErrorPage";
+import {Globalstyle} from "./Globalstyle";
+import {ThemeProvider} from ".styled-components/";
+const App = () => {
+  const theme={
+    colors: {
+      bg: "#000",
+    },
+  };
+  return (
+    <ThemeProvider theme={theme}>
+   <Router>
+     <Routes>
+    <Route path="/" element={<Home />} />
+<Route path="/about" element={<About />} />
+<Route path="/products" element={<Products />} />
+<Route path="/contact" element={<Contact />} />
+<Route path="/singleproduct/:id" element={<SingleProduct />} />
+<Route path="/cart" element={<Cart />} />
+<Route path="/*" element={<ErrorPage />} />
+    </Routes>
+   </Router>
+  )
+};
+
+export default App;
