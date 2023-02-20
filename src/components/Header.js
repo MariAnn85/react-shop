476bbfe5401520cbeedf91cb44b3d209a89f5eae
MariAnn85 <75684730+MariAnn85@users.noreply.github.com>
2023-02-20 21:40:58 +0400

    Update Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,1 +1,12 @@
-import
+const Header = () => {
+  return (
+    <MainHeader>
+     <Navlink to="/">
+      <img src="./images/logo.png" alt="my" />
+     </NavLink>
+     <Nav />
+     </MainHeader>
+    );
+};
+
+const MainHeader
