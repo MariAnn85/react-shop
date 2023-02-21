10bdd330f5f70fd251ece0f08913217a2d867882
MariAnn85 <75684730+MariAnn85@users.noreply.github.com>
2023-02-21 11:41:53 +0400

    1 file modified, 1 deleted

--- App.tsx
+++ App.tsx
@@ -1,10 +1,10 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-
-import Products from "./Products";/*
-import Contact from "./Contact";
+import Home from "Home";
+import Products from "./Products";
+import Contact from "./About";
 import Cart from "./Cart";
-import SingleProduct from "./SingleProduct";*/
+import SingleProduct from "./SingleProduct";
 import ErrorPage from "./ErrorPage";
 import {Globalstyle} from "./Globalstyle";
 /*
