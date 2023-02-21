36e3e9a91de6294400bf695317b6cabbb2922eb0
MariAnn85 <75684730+MariAnn85@users.noreply.github.com>
2023-02-21 11:37:39 +0400

    4 files added, 5 deleted

--- src/Products.js
+++
@@ -1,5 +0,0 @@
-import React from "react";
-import styled from "styled-components";
-const Products = () =>  {
-  return <h1>Products</h1>
-};
--- src/SingleProduct.js
+++
@@ -1,5 +0,0 @@
-import styled from "styled-components";
-
-const SingleProduct = () =>  {
-  return <h1>SingleProduct</h1>
-};
